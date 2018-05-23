e4be2a51c943bdd78210208d0e3ab1a5c1de7752
rprominski <rafapr@onet.pl>
2018-05-23 20:14:15 +0200

    Obsługa wciśnięć przycisków 'save' i 'clear'.

--- src/PatternCreator.java
+++ src/PatternCreator.java
@@ -1,22 +1,36 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import static javax.swing.GroupLayout.Alignment.CENTER;
 
-public class PatternCreator extends JFrame {
+public class PatternCreator extends JFrame implements ActionListener{
 
     JButton save = new JButton("save");
     JButton load = new JButton("load");
     JButton clear = new JButton("clear");
     JButton chooseFolder = new JButton("choose folder");
-    JTextField path = new JTextField("Wybierz folder do zapisu");
+    JTextField path = new JTextField("Choose folder where save image");
     PaintingField paintingField = new PaintingField();
 
     public PatternCreator(){
-        createLayout();
+        save.addActionListener(this);
+        load.addActionListener(this);
+        clear.addActionListener(this);
+        chooseFolder.addActionListener(this);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        createLayout();
     }
 
     public void createLayout(){
@@ -53,4 +67,20 @@ public class PatternCreator extends JFrame {
         pack();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == save){
+            ImageLoader.saveImage("C:\\Users\\Rav\\Desktop\\saved.jpg",paintingField.getImage());
+            paintingField.clear();
+        }
+        if(e.getSource() == load){
+           // paintingField.setImage(ImageLoader.loadImage("C:\\Users\\Rav\\Desktop\\saved.png"));
+        }
+        if(e.getSource() == clear){
+            paintingField.clear();
+        }
+        if(e.getSource() == chooseFolder){
+            System.out.println("chooseFolder");
+        }
+    }
 }
