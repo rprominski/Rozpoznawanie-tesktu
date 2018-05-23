b7814fc7e73d03884905f30bc0463de4b8b0179f
rprominski <rafapr@onet.pl>
2018-05-23 16:48:11 +0200

    Dodane rysowanie linii po przejachaniu po polu z wciśniętym LPM.

--- src/PaintingField.java
+++ src/PaintingField.java
@@ -1,18 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
-public class PaintingField extends JLabel {
-
+public class PaintingField extends JPanel implements MouseMotionListener {
+    int x,y,oldX, oldY;
+    Image image;
+    Graphics2D g2D;
     public PaintingField(){
-        setOpaque(true);
-        setBackground(Color.WHITE);
-        setPreferredSize((new Dimension(100,100)));
-        setVisible(true);
+        setPreferredSize((new Dimension(500,500)));
+        addMouseMotionListener(this);
     }
 
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        if(g2D!=null)
+        {
+            g2D.drawLine(oldX,oldY,x,y);
+            repaint();
+            oldX = x;
+            oldY = y;
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        oldX = e.getX();
+        oldY = e.getY();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        if(image==null)
+        {
+            image=createImage(getSize().width, getSize().height);
+            g2D = (Graphics2D) image.getGraphics();
+            clear();
+        }
+
+        g.drawImage(image,0,0,null);
+    }
+
+    public void clear(){
+        g2D.setPaint(Color.white);
+        g2D.fillRect(0, 0,getSize().width, getSize().height);
+        g2D.setPaint(Color.black);
+        g2D.setStroke(new BasicStroke(5));
+    }
 }
