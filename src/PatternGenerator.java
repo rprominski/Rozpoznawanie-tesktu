742250ccefdeec7af21d1e687c47c4dad3af75be
rprominski <rafapr@onet.pl>
2018-05-26 21:20:38 +0200

    Zmiana nazw zmiennych.

--- src/PatternGenerator.java
+++ src/PatternGenerator.java
@@ -5,19 +5,19 @@ import java.util.Map;
 public class PatternGenerator {
     private static final String patternsFolderPath = "C:\\Users\\Rav\\Pictures\\patterns" ;
 
-    public HashMap<String, LetterMatrix> generateAllPatterns(){
-        HashMap<String,LetterMatrix> alfabet=new HashMap<String,LetterMatrix>();
+    public HashMap<String, Pattern> generateAllPatterns(){
+        HashMap<String, Pattern> alfabet=new HashMap<String, Pattern>();
         final File patternsFolder = new File(patternsFolderPath);
         File[] letterMatrixFolders = patternsFolder.listFiles();
 
         for(File folder : letterMatrixFolders){
             if(folder.isDirectory()){
-                alfabet.put(folder.getName(),new LetterMatrix(patternsFolder+"\\"+folder.getName()));
+                alfabet.put(folder.getName(),new Pattern(patternsFolder+"\\"+folder.getName()));
             }
         }
 
         for(Map.Entry it : alfabet.entrySet()){
-            System.out.println(it.getKey()+" "+it.getValue());
+           // System.out.println(it.getKey()+" "+it.getValue());
         }
         return alfabet;
     }
