f5e1ffb6b4f630f01a5aa3e68ff34d60e277485e
rprominski <rafapr@onet.pl>
2018-05-26 21:21:55 +0200

    Dodana funkcja zamieniająca 3 liczby na jedną reprezenrującą kolor RGB.

--- src/Color.java
+++ src/Color.java
@@ -9,6 +9,14 @@ public class Color {
         blue = (rgb) & 0x000000FF;
     }
 
+    static public int getIntFromColor(int Red, int Green, int Blue){
+        Red = (Red << 16) & 0x00FF0000;
+        Green = (Green << 8) & 0x0000FF00;
+        Blue = Blue & 0x000000FF;
+
+        return 0xFF000000 | Red | Green | Blue;
+    }
+
     public Color() {
     }
 
