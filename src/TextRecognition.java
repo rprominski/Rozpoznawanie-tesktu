d50c5f680aa7fcb3c0600bdaefeffab81e5378ea
rprominski <rafapr@onet.pl>
2018-05-27 13:52:53 +0200

    Sparametryzowanie głównej funkcji.
    Dla pierwszego argumentu "create" otwiera Kreatora wzorców.
    Drugi parametr jest opcjonalny i wskazuje folder do którego będą wzorce
    będą domyślnie zapisywane.
    
    Dla pierwszego parametru "test" zostaną przetestowane obrazy z folderu
    "testImages" na podstawie wzorców z folderu "patterns". Drugi i trzeci
    parametr są opcjonalne i oznaczają ścieżki do folderu z
    testowanymi obrazami i wzorcami inne niż domyślne.

--- src/TextRecognition.java
+++ src/TextRecognition.java
@@ -5,13 +5,21 @@ import java.util.*;
 public class TextRecognition {
 
     public static void main(String args[]) {
-        EventQueue.invokeLater(() -> {
-            String path = "C:\\Users\\Rav\\Desktop\\M";
-            TextRecognizer textRecognizer = new TextRecognizer();
-            for(File folder : new File(path).listFiles()) {
-            System.out.println(folder.getName() + " " + textRecognizer.recognize(ImageLoader.loadImage(path + "\\"+folder.getName())));
-            }
-        });
-    }
+        if(args[0].equals("create")) {
+            EventQueue.invokeLater(() -> {
 
+                    String patternsFolderPath = (args.length>1 ? args[1] : "patterns");
+                    PatternCreator patternCreator = new PatternCreator(patternsFolderPath);
+            });
+        }
+        if(args[0].equals("test")) {
+            String testedImagesPath = (args.length>1 ? args[1] : "testImages");
+            String patternsFolderPath = (args.length>2 ? args[2] : "patterns");
+            TextRecognizer textRecognizer = new TextRecognizer(patternsFolderPath);
+
+            for (File folder : new File(testedImagesPath).listFiles()) {
+                System.out.println(folder.getName() + " " + textRecognizer.recognize(ImageLoader.loadImage(testedImagesPath + "\\" + folder.getName())));
+            }
+        }
+    }
 }
